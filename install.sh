b560b770ee133d31d315c54e9b10e95961488d3d
ezClap <itssrapids@gmail.com>
2026-03-05 12:45:10 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+wget https://www.passmark.com/downloads/BurnInTest_Linux_x86-64.tar.gz
+tar -xvf BurnInTest_Linux_x86-64.tar.gz
+cd burnintest
+sudo yum install qt5-qtsensors -y
+sudo yum install qt5-qtlocation -y
+./bit_gui_x64
