01ba98624a466738641aed5054d6e162e0a426aa
ezClap <itssrapids@gmail.com>
2026-03-05 12:43:34 +0000

    Update BIT_Install.sh

--- BIT_Install.sh
+++ BIT_Install.sh
@@ -1,5 +1,5 @@
 wget https://www.passmark.com/downloads/BurnInTest_Linux_x86-64.tar.gz
-tar -xvf BurnInTest
+tar -xvf BurnInTest_Linux_x86-64.tar.gz
 cd burnintest
 sudo yum install qt5-qtsensors -y
 sudo yum install qt5-qtlocation -y
